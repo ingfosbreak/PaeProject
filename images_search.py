2ccd26ea83182c43fa71badb1bcfd243bbbc9118
ParnThanatibordee <parn8946@gmail.com>
2022-12-14 01:15:40 +0700

    implement fetch_images

--- images_search.py
+++ images_search.py
@@ -1,8 +1,33 @@
 from google_images_search import GoogleImagesSearch
 from dotenv import load_dotenv, dotenv_values
+import os
 
-load_dotenv()  # take environment variables from .env.
+if os.path.exists(".env"):
+  load_dotenv()  # take environment variables from .env.
+else:
+  print(".env file missing")
 
-config = dict(dotenv_values(".env"))
+DEVELOPER_KEY = os.environ.get("GCS_DEVELOPER_KEY")
+CX = os.environ.get("GCS_CX")
 
-# print(config)
+# print(DEVELOPER_KEY, CX)
+
+# gis = GoogleImagesSearch(DEVELOPER_KEY, CX)
+
+
+def fetch_images(searchfor):
+  with GoogleImagesSearch(DEVELOPER_KEY, CX) as gis:
+    _search_params = {"q": searchfor, 
+        "num": 15, 
+        "safe": "high", 
+        "fileType": "jpg",
+        "imgType": "photo",
+        "rights": "cc_publicdomain" 
+        }
+    gis.search(search_params=_search_params)
+    return gis.results()
+
+
+image0 = fetch_images("monkey")[0]
+print(image0.url)
+# print(image0.referrer_url)
