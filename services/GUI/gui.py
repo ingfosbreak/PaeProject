91e2ca1cf767f64babd6f1b9c04d3fe6a169ea19
ingfosbreak <ingfosbreak@outlook.com>
2022-12-14 17:48:10 +0700

    Update: draft GUI

--- services/GUI/gui.py
+++ services/GUI/gui.py
@@ -1,21 +1,57 @@
 from tkinter import *
 
+def round_rectangle(root, x1, y1, x2, y2, radius=25, **kwargs): # Creating a rounded rectangle
+        
+        points = [x1+radius, y1,
+                x1+radius, y1,
+                x2-radius, y1,
+                x2-radius, y1,
+                x2, y1,
+                x2, y1+radius,
+                x2, y1+radius,
+                x2, y2-radius,
+                x2, y2-radius,
+                x2, y2,
+                x2-radius, y2,
+                x2-radius, y2,
+                x1+radius, y2,
+                x1+radius, y2,
+                x1, y2,
+                x1, y2-radius,
+                x1, y2-radius,
+                x1, y1+radius,
+                x1, y1+radius,
+                x1, y1]
+
+        return root.create_polygon(points, **kwargs, smooth=True, fill="#DEE5E5")
+
+
 main = Tk()
-# main.title("PaeProgram")
-main.geometry("1000x600")
+main.title("PaeProgram")
+main.geometry("1000x550")
 
 
-# frame = Frame(main)
-# frame.pack()
-
-
-leftFrame = Frame(main, bg="red")
+leftFrame = Frame(main, bg="#FFFFFF",width=400,height=550)
 leftFrame.pack(side=LEFT)
-rightFrame = Frame(main, bg="white")
+rightFrame = Frame(main, bg="#007ea7",width=800,height=550)
 rightFrame.pack(side= RIGHT)
 
-Label(leftFrame).pack(pady=600,padx=200)
-Label(rightFrame).pack(pady=600,padx=800)
+canvas = Canvas(leftFrame, bg="#FFFFFF", width=350,height=550,highlightthickness=0)
+canvas.pack(padx=10,pady=25)
+round_rectangle(canvas,0, 0, 350, 500, radius=70)
+# canvas = Canvas(leftFrame, bg="grey", highlightthickness=0)
+# canvas.pack(fill=BOTH, expand=1)
+
+
+Label(canvas, text="Position 1 : x=0, y=0", bg="white", fg="black", font=('Times',24)).place(x=50, y=200)
+
+
+
+
+
+
+
+
 
 # firstName = Entry(main)
 # lastName = Entry(main)
@@ -24,3 +60,9 @@ Label(rightFrame).pack(pady=600,padx=800)
 # lastName.grid(row=1, column=1)
 
 main.mainloop()
+
+
+
+
+
+
